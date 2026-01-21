59636df1bf0285ec0c6aa594118feb61a64bd269
bradley200813 <bradgaming131@gmail.com>
2026-01-21 15:23:45 -0500

    Fix branch name to master

--- github-install.lua
+++ github-install.lua
@@ -15,7 +15,7 @@
 -- ===========================================
 local GITHUB_USER = "bradley200813"
 local GITHUB_REPO = "minecraft"
-local BRANCH = "main"
+local BRANCH = "master"
 -- ===========================================
 
 local BASE_URL = "https://raw.githubusercontent.com/" .. GITHUB_USER .. "/" .. GITHUB_REPO .. "/" .. BRANCH .. "/colony/"
